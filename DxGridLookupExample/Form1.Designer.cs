f0ccd458c01684cd7346705cede3e2c502913ca9
Jacek Czapla <jczapla@asit.pl>
2022-08-08 11:53:53 +0200

    init

---
+++ DxGridLookupExample/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace DxGridLookupExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bsCars = new System.Windows.Forms.BindingSource(this.components);
+            this.bsColors = new System.Windows.Forms.BindingSource(this.components);
+            this.gcCars = new DevExpress.XtraGrid.GridControl();
+            this.gvCars = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colMarka = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colOpis = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colColorId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repColors = new DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsCars)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsColors)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gcCars)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCars)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repColors)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bsCars
+            // 
+            this.bsCars.DataSource = typeof(DxGridLookupExample.Car);
+            // 
+            // bsColors
+            // 
+            this.bsColors.DataSource = typeof(DxGridLookupExample.Color);
+            // 
+            // gcCars
+            // 
+            this.gcCars.DataSource = this.bsCars;
+            this.gcCars.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gcCars.Location = new System.Drawing.Point(0, 0);
+            this.gcCars.MainView = this.gvCars;
+            this.gcCars.Name = "gcCars";
+            this.gcCars.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repColors});
+            this.gcCars.Size = new System.Drawing.Size(759, 434);
+            this.gcCars.TabIndex = 1;
+            this.gcCars.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvCars});
+            // 
+            // gvCars
+            // 
+            this.gvCars.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colId,
+            this.colMarka,
+            this.colOpis,
+            this.colColorId});
+            this.gvCars.GridControl = this.gcCars;
+            this.gvCars.Name = "gvCars";
+            this.gvCars.OptionsView.NewItemRowPosition = DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.Top;
+            // 
+            // colId
+            // 
+            this.colId.FieldName = "Id";
+            this.colId.Name = "colId";
+            this.colId.Visible = true;
+            this.colId.VisibleIndex = 0;
+            // 
+            // colMarka
+            // 
+            this.colMarka.FieldName = "Marka";
+            this.colMarka.Name = "colMarka";
+            this.colMarka.Visible = true;
+            this.colMarka.VisibleIndex = 1;
+            // 
+            // colOpis
+            // 
+            this.colOpis.FieldName = "Opis";
+            this.colOpis.Name = "colOpis";
+            this.colOpis.Visible = true;
+            this.colOpis.VisibleIndex = 2;
+            // 
+            // colColorId
+            // 
+            this.colColorId.ColumnEdit = this.repColors;
+            this.colColorId.FieldName = "ColorId";
+            this.colColorId.Name = "colColorId";
+            this.colColorId.Visible = true;
+            this.colColorId.VisibleIndex = 3;
+            // 
+            // repColors
+            // 
+            this.repColors.AutoHeight = false;
+            this.repColors.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repColors.Columns.AddRange(new DevExpress.XtraEditors.Controls.LookUpColumnInfo[] {
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("Id", "Id", 33, DevExpress.Utils.FormatType.Numeric, "", true, DevExpress.Utils.HorzAlignment.Far),
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("Name", "Name", 120, DevExpress.Utils.FormatType.None, "", true, DevExpress.Utils.HorzAlignment.Near)});
+            this.repColors.DataSource = this.bsColors;
+            this.repColors.DisplayMember = "Name";
+            this.repColors.KeyMember = "Id";
+            this.repColors.Name = "repColors";
+            this.repColors.ValueMember = "Id";
+            this.repColors.QueryPopUp += new System.ComponentModel.CancelEventHandler(this.repColors_QueryPopUp);
+            this.repColors.CloseUp += new DevExpress.XtraEditors.Controls.CloseUpEventHandler(this.repColors_CloseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 434);
+            this.Controls.Add(this.gcCars);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bsCars)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsColors)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gcCars)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCars)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repColors)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bsCars;
+        private System.Windows.Forms.BindingSource bsColors;
+        private DevExpress.XtraGrid.GridControl gcCars;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvCars;
+        private DevExpress.XtraGrid.Columns.GridColumn colId;
+        private DevExpress.XtraGrid.Columns.GridColumn colMarka;
+        private DevExpress.XtraGrid.Columns.GridColumn colOpis;
+        private DevExpress.XtraGrid.Columns.GridColumn colColorId;
+        private DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit repColors;
+    }
+}
+
---
+++ DxGridLookupExample/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DxGridLookupExample
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        List<Car> Cars = new List<Car>();
+        List<Color> Colors;
+        List<ColorMarka> ColoryDlaMarki;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+
+
+            Colors = CreateColors();
+            ColoryDlaMarki = CreateKoloryDlaMarki();
+
+            bsCars.DataSource = Cars;
+            bsColors.DataSource = Colors;
+        }
+
+        private List<Color> CreateColors()
+        {
+            return new List<Color>()
+            {
+                new Color { Id = 1, Name = "Czerwony" },
+                new Color { Id = 2, Name = "Zielony" },
+                new Color { Id = 3, Name = "Zółty" },
+                new Color { Id = 4, Name = "Czarny" }
+            };
+        }
+        private List<ColorMarka> CreateKoloryDlaMarki()
+        {
+            return new List<ColorMarka> {
+                new ColorMarka { Marka="Fiat", ColorId = 1},
+                new ColorMarka { Marka="Fiat", ColorId = 2},
+                new ColorMarka { Marka="Fiat", ColorId = 3},
+                new ColorMarka { Marka="BMW", ColorId = 3},
+                new ColorMarka { Marka="BMW", ColorId = 4},
+                
+            };
+        }
+        private List<Color> GetColorsForMarka(string marka)
+        {
+            // pobranie kolorów dostępnych dla marki po nazwie marki, jakieś zapytanie dl DB
+            var colorIds = ColoryDlaMarki.Where(x => x.Marka == marka).Select(x => x.ColorId).ToList();
+            return Colors.Where(x => colorIds.Contains(x.Id)).ToList();
+        }
+
+        private void repColors_QueryPopUp(object sender, CancelEventArgs e)
+        {
+            // filtrowanie dostępnych kolorów wg zawartośći pola Marka
+
+            string marka = (string)gvCars.GetRowCellValue(gvCars.FocusedRowHandle, colMarka);
+            //jeśli nie ma wpisanej marki to nic nie pokazujemy
+            if (string.IsNullOrEmpty(marka))
+                e.Cancel = true;
+
+            bsColors.DataSource = GetColorsForMarka(marka);
+        }
+
+        private void repColors_CloseUp(object sender, DevExpress.XtraEditors.Controls.CloseUpEventArgs e)
+        {
+            //załadowanie WSZYSTKICH kolorów po zamknięciu listy kolorów
+            bsColors.DataSource = Colors;
+        }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Marka { get; set; }
+        public string Opis { get; set; }
+        public int ColorId { get; set; }
+    }
+
+    public class ColorMarka
+    {
+        public string Marka { get; set; }
+        public int ColorId { get; set; }
+    }
+
+    public class Color
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
